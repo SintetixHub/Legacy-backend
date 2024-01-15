90f29a0470de3f85c06804cc8d78a0a4a6e158a2
Ebanx3 <esteban2santos@gmail.com>
2024-01-15 11:21:14 -0300

    colores at console :D

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ import "./models/blog.js";
 (async () => {
   try {
     await sequelize.sync();
+    console.log("\x1b[34m", "\n", "Connected to db!");
     app.listen(config.PORT, () =>
-      console.log(`Server up! -> http://localhost:${config.PORT} 👻 `)
+      console.log(
+        " Server up! ->",
+        "\x1b[1m",
+        "\x1b[37m",
+        "\x1b[4m",
+        `http://localhost:${config.PORT}`,
+        "\x1b[0m",
+        "\n"
+      )
     );
   } catch (err) {
     console.log(err);
