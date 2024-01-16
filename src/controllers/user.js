2d4a247b51ecced9342935fecf371376bf548db3
Ebanx3 <esteban2santos@gmail.com>
2024-01-16 04:01:24 -0300

    habemus encrypted password

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
 import { getByName, create } from "../models/user.js";
 import { validateData } from "../services/joi.js";
+import { hashPassword } from "../services/bcrypt.js";
 
 const login = async (req, res) => {
   try {
@@ -21,22 +22,31 @@ const login = async (req, res) => {
 const signup = async (req, res) => {
   try {
     const val = await validateData("user", req.body);
-    // console.log(val);
+
     if (val.name === "ValidationError") {
       return res.status(400).json({ success: false, message: val.message });
     }
 
+    // const newData = {
+    //     username:val.username,
+    //     email: val.email,
+    //     password:
+    // }
+    val.password = await hashPassword(val.password);
+
     const newUser = await create(val);
 
-    console.log(newUser.parent.detail);
+    if (newUser.name === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .json({ success: false, message: newUser.parent.detail });
+    }
 
     res
       .status(200)
       .json({ success: true, message: "user created succesfully" });
   } catch (error) {
-    // if (error.name && error.name === "ValidationError") {
-    //   return res.status(400).json({ success: false, message: error.error });
-    // }
+    console.log(error);
     res.status(500).json({ success: false, message: "server error" });
   }
 };
